cc28cdc7953e324fbe1aca8d0638e67e534f6f10
Heat Koemnak <heatkimnak@gmail.com>
2023-08-02 00:46:13 +0700

    asxas

--- APL-S2/Assignment4/ch4_ex1.java
+++ APL-S2/Assignment4/ch4_ex1.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
 
 public class ch4_ex1 {
+    public static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
         String[] months = { "January", "February", "March", "April", "May", "June", "July", "August", "September",
                 "October", "November", "December" };
         int[] dom = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-        Scanner scan = new Scanner(System.in);
+        
         System.out.print("Enter an integer between 1 and 12: ");
         int input = scan.nextInt();
 
